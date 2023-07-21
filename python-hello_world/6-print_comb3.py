459362994bcdcc617c1bfb8828118b183a20bb90
NormanDee <newman.demba@uhuruinnovative.com>
2023-07-20 21:00:28 -0700

    program that prints all possible different combinations of two digits.

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,6 @@
 for num1 in range(10):
     for num2 in range(num1 + 1, 10):
-        print("{:02d}, {:02d}".format(num1, num2), end=", " if num1 < 8 else "\n")
+        if num1 == 8 and num2 == 9:
+            print("{:01d}{:01d}".format(num1, num2))
+        else:
+            print("{:01d}{:01d}".format(num1, num2), end=", ")
