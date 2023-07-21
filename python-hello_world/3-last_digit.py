e2b16f330831da7208209850bd835b56d8d495b0
NormanDee <newman.demba@uhuruinnovative.com>
2023-07-20 22:23:02 -0700

    The last digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-# Print the random number
-#Sprint("The number:", number)
 
-# Check if the number is positive, zero, or negative and print the result
 if number > 0:
     print("is positive")
 elif number == 0:
