56906d8f7e6927f20c7218cdf6f3784fdadae479
NormanDee <newman.demba@uhuruinnovative.com>
2023-07-24 19:43:33 -0700

    Import a simple function from a simple file

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    return a + b
+
+# Assign values to variables a and b
+a = 1
+b = 2
+
+# Call the add function and print the result
+result = add(a, b)
+print("{} + {} = {}".format(a, b, result))
