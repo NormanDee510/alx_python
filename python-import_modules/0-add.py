dcf1875247578d9be255f1f3061252f3d74283ba
NormanDee <newman.demba@uhuruinnovative.com>
2023-07-24 19:53:20 -0700

    Import a simple function from a simple file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,3 +1,5 @@
+from add_0 import add
+
 if __name__ == "__main__":
     def add(a, b):
         return a + b
@@ -9,3 +11,6 @@ b = 2
 # Call the add function and print the result
 result = add(a, b)
 print("{} + {} = {}".format(a, b, result))
+
+# Print the output with string formatting
+print("FAKE add() => {} - {}".format(a, b))
