dfa61ea3b2376adae9bd8eeaf5934d9992adbf13
NormanDee <newman.demba@uhuruinnovative.com>
2023-07-26 09:39:03 -0700

    Import a simple function from a simple file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -6,21 +6,7 @@ def main():
     b = 2
   
     result = add(a, b)
-    a = 10
-    b = 30
-    result = add(a, b)
-
-    a = -10
-    b = 30
-    result = add(a, b)
-
-    a = -10
-    b = -30
-    result = add(a, b)
     
-    print("FAKE add() => {} - {}".format(a, b))
-    print("FAKE add() => {} - {}".format(a, b))
-    print("FAKE add() => {} - {}".format(a, b))
     print("FAKE add() => {} - {}".format(a, b))
 
 if __name__ == "__main__":
