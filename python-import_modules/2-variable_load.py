e3b4302c997f09650c80b577df340ad2e5eb007e
NormanDee <newman.demba@uhuruinnovative.com>
2023-07-29 15:42:15 -0700

    Everything can be imported

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,21 +1,5 @@
-import importlib.util
-
-def import_variable_from_file(filepath, variable_name):
-    spec = importlib.util.spec_from_file_location("variable_module", filepath)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-
-    if hasattr(module, variable_name):
-        return getattr(module, variable_name)
-    else:
-        return None
-
 if __name__ == "__main__":
-    filepath = "variable_load_2.py"
-    variable_name = "a"
+    """Print the value of variable a from variable_load_2."""
+    from variable_load_2 import a
 
-    a = import_variable_from_file(filepath, variable_name)
-    if a is not None:
-        print(f"a = {a}")
-    else:
-        print(f"a missing")
+print(a)
