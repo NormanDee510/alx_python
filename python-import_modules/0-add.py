860390823b8412d024cfd9a6fe7293bfa79becc7
NormanDee <newman.demba@uhuruinnovative.com>
2023-07-29 15:26:21 -0700

    Import a simple function from a simple file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,13 +1,9 @@
-from add_0 import add
+#!/usr/bin/python3
 
-def main():
-    
+if name == "main":
+    """Print the sum of 1 and 2."""
+    from add_0 import add
+
     a = 1
     b = 2
-  
-    result = add(a, b)
-    
-    print("FAKE add() => {} - {}".format(a, b))
-
-if __name__ == "__main__":
-    main()
+    print("{} + {} = {}".format(a, b, add(a, b)))
