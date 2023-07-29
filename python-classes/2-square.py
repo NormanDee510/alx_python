719a75d8c0adf2d29f0bc592c2c30e45f991a6d9
NormanDee <newman.demba@uhuruinnovative.com>
2023-07-29 11:42:29 -0700

    Area of a square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,11 +1,12 @@
-try:
-    my_square = Square(3)
-    print("Area:", my_square.area())
-except TypeError as e:
-    print(e)
+class Square:
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
 
-try:
-    my_square = Square(-1)
-    print("Area:", my_square.area())
-except ValueError as e:
-    print(e)
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
