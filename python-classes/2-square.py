6459b49603039e4362c7ae8ef785432348c0156c
NormanDee <newman.demba@uhuruinnovative.com>
2023-07-31 22:04:36 -0700

    Area of a square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,12 +1,23 @@
 class Square:
+
+    """ class Square.
+    intializes a square attribut
+    """
+
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        """intialize attributes
+        Args:
+            size (int): size of a square
+        Returns: none
+        """
+        if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
+        """returns the area of a square"""
         return self.__size ** 2
 
 
