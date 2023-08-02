babca7cfa2fb5a33ab7076ab9fa6b5bd3e0d314c
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-02 10:47:20 -0700

    Size validation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,24 +1,9 @@
-#!/usr/bin/python3
-"""module Square,
-
-checks weather the value of size is integer or not
-"""
-
-
 class Square:
-
-    """defines a Square class"""
-
     def __init__(self, size=0):
-        """instantation of private attribute of class
-
-        Args:
-            size: size
-            """
-
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
+
+        
