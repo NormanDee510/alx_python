3836b014e0ecfd4217b946c31c2c37bb39b8dca1
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-02 14:20:02 -0700

    Square #1

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""python inheritance.
+
+defines and assigns value for parameter
+
+"""
+class BaseGeometry:
+    """ My BaseGeometry class. """
+
+    def area(self):
+        """Raises an Exception with the message area() is not implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates the 'value' argument.
+
+        Args:
+            name (str): The name of the value being validated.
+            value (int): The value to be validated.
+
+        Raises:
+            TypeError: If the 'value' is not an integer.
+            ValueError: If the 'value' is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """ My Rectangle class that inherits from BaseGeometry. """
+
+    def __init__(self, width, height):
+        """Instantiates the Rectangle with width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        """Returns the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns a string representation of the Rectangle."""
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
+class Square(Rectangle):
+    """ My Square class that inherits from Rectangle. """
+
+    def __init__(self, size):
+        """Instantiates the Square with size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Returns a string representation of the Square."""
+        return f"[Square] {self._Rectangle__width}/{self._Rectangle__height}"
