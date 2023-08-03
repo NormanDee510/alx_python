f15f11c93a9fa0af67d2774e4bcc37d9bc7ffe91
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-02 22:15:51 -0700

    Only sub class of

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -4,18 +4,18 @@ python inheritance.
 defines and assigns value for parameter
 
 """
-
 def inherits_from(obj, a_class):
-    """
-    Check if the object is an instance of a class that inherited (directly or indirectly) from the specified class.
-        Args:
-        obj: The object to check.
-        a_class: The class to compare against.
-        Returns:
-        True if the object is an instance of a class that inherited from the specified class; otherwise, False.
+    """Determines if obj is an instance of a class that
+    inherited from a_class
 
-    """    
-    return issubclass(type(obj), a_class) and type(obj) is not a_class
+    Args:
+        - obj: object to look at
+        - a_class: class to evaluate
+
+    Returns: True or False
+    """
+
+    return isinstance(obj, a_class) and type(obj) != a_class
 
 
 
