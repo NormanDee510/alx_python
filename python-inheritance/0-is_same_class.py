8edd67c46388fd0f1d2dd1dc99d5a2f00e7685a9
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-02 21:32:15 -0700

    Only sub class of

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -6,7 +6,6 @@ defines and assigns value for parameter
 """
 def is_same_class(obj, a_class):
     """Check if the object is exactly an instance of the specified class.
-
     Args:
         obj: The object to check.
         a_class: The class to compare against.
