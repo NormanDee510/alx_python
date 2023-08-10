9ca1ec33dc39e5a8799a726f7d9811dda99145e7
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-10 13:01:09 -0700

    My GitHub!

---
+++ python-network_1/6-my_github.py
@@ -0,0 +1,48 @@
+"""
+Use GitHub API with Basic Authentication to display user id.
+
+This script takes GitHub credentials (username and personal access token) as input,
+uses Basic Authentication with the personal access token, and displays the user id.
+
+Usage:
+    python script.py <username> <personal_access_token>
+
+Args:
+    username (str): Your GitHub username.
+    personal_access_token (str): Your GitHub personal access token.
+
+Example:
+    python script.py papamuziko cisfun
+"""
+
+import requests
+import sys
+
+def get_github_user_id(username, access_token):
+    """
+    Use GitHub API with Basic Authentication to display user id.
+
+    Args:
+        username (str): Your GitHub username.
+        access_token (str): Your GitHub personal access token.
+    """
+    url = f"https://api.github.com/user"
+    headers = {"Authorization": f"Basic {access_token}"}
+
+    try:
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            user_data = response.json()
+            print(user_data.get("id"))
+        else:
+            print("None")
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        username = sys.argv[1]
+        personal_access_token = sys.argv[2]
+        get_github_user_id(username, personal_access_token)
+    else:
+        print("Usage: python script.py <username> <personal_access_token>")
