6fea96259f41d3c93a6074e568c3d358dd059491
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-10 12:06:21 -0700

    Response header value #1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,7 +1,30 @@
+"""
+Fetch X-Request-Id from HTTP response header and display results.
+
+This script takes a URL as input, sends a GET request to the URL, and displays
+the value of the 'X-Request-Id' variable in the response header. It also
+provides additional information based on the response and any redirections.
+
+Usage:
+    python script.py <URL>
+
+Args:
+    URL (str): The URL to send the GET request to.
+
+Example:
+    python script.py http://0.0.0.0:5050
+"""
+
 import requests
 import sys
 
 def get_x_request_id(url):
+    """
+    Fetch X-Request-Id from the response header and display results.
+
+    Args:
+        url (str): The URL to send the GET request to.
+    """
     try:
         response = requests.get(url)
         x_request_id = response.headers.get('X-Request-Id')
