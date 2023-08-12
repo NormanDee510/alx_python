635353707d968c832bc40e15f6e3e2b87d0c7f87
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-12 09:46:50 -0700

    Geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -5,8 +5,5 @@ defines and assigns value for parameter
 
 """
 class BaseGeometry:
-    def __dir__(cls) -> None:
-        # get list of all attributes for this class and exclude __init_subclass
-        attributes = super().__dir__()
-
-        return [attribute for attribute in attributes if attribute != '__init_subclass__']
+    """ My BaseGeometry class. """
+    pass
