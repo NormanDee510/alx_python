5704f8c057bf14456762149081f1fb8b0ddeac73
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-12 19:12:12 -0700

    Search API

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -18,26 +18,24 @@ Example:
 import requests
 import sys
 
-def search_user_by_letter(letter):
-    base_url = "http://0.0.0.0:5000/search_user"
-    payload = {'q': letter}
-    
-    response = requests.post(base_url, data=payload)
-    
-    try:
-        response_json = response.json()
-        if response_json and 'id' in response_json and 'name' in response_json:
-            print(f"[{response_json['id']}] {response_json['name']}")
-        else:
-            print("No result")
-    except ValueError:
-        print("Not a valid JSON")
+def fetch_and_display_response(url):
+    """
+    Send a request to a URL, display response body, and handle error codes.
+
+    Args:
+        url (str): The URL to send the request to.
+    """
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
-        letter = sys.argv[1]
+        url = sys.argv[1]
+        fetch_and_display_response(url)
     else:
-        letter = ""
-    
-    search_user_by_letter(letter)
+        print("Usage: python script.py <URL>")
+
         
