689ce51249b97dcfadf85d682858ee15aa973816
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-20 14:03:01 -0700

    SQL Injection...

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,11 +6,11 @@ import MySQLdb
 def safe_filter_states(username, password, database, state_name):
     # Connect to the MySQL server
     db = MySQLdb.connect(
-    host="localhost", 
-    port=3306, 
-    user=username, 
-    passwd=password, 
-    db=database
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
     )
 
     # Create a cursor
@@ -18,7 +18,7 @@ def safe_filter_states(username, password, database, state_name):
 
     # Create a parameterized query to retrieve states matching the state name
     query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    
+
     # Execute the query with a tuple containing the state name as the parameter
     cursor.execute(query, (state_name,))
 
@@ -36,13 +36,14 @@ def safe_filter_states(username, password, database, state_name):
 if __name__ == "__main__":
     # Check for correct number of arguments
     if len(sys.argv) != 5:
-        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        print("Usage: {} <username> <password>"
+              "<database> <state_name>".format(sys.argv[0]))
         sys.exit(1)
     
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     state_name = sys.argv[4]
-    
+
     # Call the function to filter and display the states
     safe_filter_states(username, password, database, state_name)
