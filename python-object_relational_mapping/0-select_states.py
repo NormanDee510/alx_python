d658d732fd99b0aea99da85bf88716c81c053668
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-20 11:50:19 -0700

    Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,24 +3,14 @@ import sys
 import MySQLdb
 
 """
-This script connects to a MySQL database and retrieves states' information from the 'states' table.
-It takes three arguments: mysql username, mysql password, and database name.
+A script that takes 3 arguments: mysql username, mysql password, and database name.
 """
 
 def get_states(username, password, database):
-    """
-    Retrieves and displays information about states from the database.
-
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database (str): Name of the database.
-
-    Returns:
-        None
-    """
     # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=username, passwd=password, db=database
+    )
 
     # Create a cursor
     cursor = db.cursor()
@@ -40,16 +30,14 @@ def get_states(username, password, database):
     db.close()
 
 if __name__ == "__main__":
-    """ Main entry point of the script."""
-    
-    # Check for correct number of arguments
+    # Check for the correct number of arguments
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
-    
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    
+
     # Call the function to get and display the states
     get_states(username, password, database)
