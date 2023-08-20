434a4bb48d217a5c70dc88f9810480cd960361bc
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-20 14:15:30 -0700

    Cities by states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -41,7 +41,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
-    
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
