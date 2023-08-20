bf0e62f3ea72f264c3c71cbc79b1ca284504b7f6
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-20 13:12:58 -0700

    Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -4,15 +4,12 @@ import MySQLdb
 
 
 def filter_states(username, password, database):
-
-
     db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
     # Create a cursor
     cursor = db.cursor()
 
     # Execute the query to retrieve states starting with 'N'
-    cursor.execute("SELECT * FROM states WHERE LOWER(name)"
-                   "LIKE 'n%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE LOWER(name) LIKE 'n%' ORDER BY id ASC")
 
     # Fetch all the results
     results = cursor.fetchall()
@@ -28,8 +25,7 @@ def filter_states(username, password, database):
 if __name__ == "__main__":
     # Check for correct number of arguments
     if len(sys.argv) != 4:
-        print("Usage: {} <username> <password>"
-              "<database>".format(sys.argv[0]))
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
 
     username = sys.argv[1]
