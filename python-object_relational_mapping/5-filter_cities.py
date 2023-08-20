712c01db4d41786f4eaf4ad635f8a342df2cf915
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-20 14:18:36 -0700

    All cities by state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -2,9 +2,16 @@
 import sys
 import MySQLdb
 
+
 def filter_cities_by_state(username, password, database, state_name):
     # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
 
     # Create a cursor
     cursor = db.cursor()
@@ -38,7 +45,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 5:
         print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
         sys.exit(1)
-    
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -46,3 +53,4 @@ if __name__ == "__main__":
     
     # Call the function to filter and display cities by state
     filter_cities_by_state(username, password, database, state_name)
+    
