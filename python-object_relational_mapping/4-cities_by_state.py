952c63671266ebec53683360ce9d6c47648f8ee1
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-20 14:14:26 -0700

    Cities by states

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -45,6 +45,6 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    
+
     # Call the function to retrieve and display cities by state
     cities_by_state(username, password, database)
