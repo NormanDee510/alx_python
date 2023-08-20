b97728c8d31300040a32d0c0d8da9628b1f7b629
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-20 12:41:54 -0700

    Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ def filter_states(username, password, database):
     cursor = db.cursor()
 
     # Execute the query to retrieve states starting with 'N'
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE LOWER(name) LIKE 'n%' ORDER BY id ASC")
 
     # Fetch all the results
     results = cursor.fetchall()
@@ -27,12 +27,12 @@ if __name__ == "__main__":
     # Check for correct number of arguments
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password>"
-              "<database>".format(sys.argv[0]))
+             "<database>".format(sys.argv[0]))
         sys.exit(1)
-    
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    
+
     # Call the function to filter and display the states
     filter_states(username, password, database)
