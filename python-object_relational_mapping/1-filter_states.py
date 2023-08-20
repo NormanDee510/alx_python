9b3b3a9a6613fcedf7757844a1fedb694a88cc79
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-20 13:09:49 -0700

    Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,6 @@ def filter_states(username, password, database):
 
 
     db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-
     # Create a cursor
     cursor = db.cursor()
 
