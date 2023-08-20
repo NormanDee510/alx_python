65c69157b97dc4c0a9b6036a5ac4b23871d96f83
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-20 13:04:53 -0700

    Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -3,9 +3,10 @@ import sys
 import MySQLdb
 
 def filter_states(username, password, database):
-    
-    # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                          passwd=password, db=database)
 
     # Create a cursor
     cursor = db.cursor()
