737d54881ef7d4dfcc2c3874a9f684b4e58c7f53
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-20 15:25:37 -0700

    Contains a

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -6,10 +6,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == "__main__":
     # Check for correct number of arguments
     if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        print("Usage: {} <username> <password>"
+              "<database>".format(sys.argv[0]))
         sys.exit(1)
     
     username = sys.argv[1]
@@ -17,7 +19,9 @@ if __name__ == "__main__":
     database = sys.argv[3]
     
     # Create an engine
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, database), pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(username, password, database), pool_pre_ping=True)
     
     # Create a session
     Session = sessionmaker(bind=engine)
@@ -27,6 +31,6 @@ if __name__ == "__main__":
     states_with_a = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
     for state in states_with_a:
         print("{}: {}".format(state.id, state.name))
-    
+
     # Close the session
     session.close()
