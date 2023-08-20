31fc640bc583e7f8b037f315b06db6f7b3516225
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-20 12:38:56 -0700

    Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -26,7 +26,8 @@ def filter_states(username, password, database):
 if __name__ == "__main__":
     # Check for correct number of arguments
     if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        print("Usage: {} <username> <password>"
+              "<database>".format(sys.argv[0]))
         sys.exit(1)
     
     username = sys.argv[1]
