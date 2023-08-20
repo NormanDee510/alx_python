b1b7e23b831746a1b80b55f7669d797c96009cf0
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-20 10:19:11 -0700

    Get all states

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,9 +1,13 @@
-#!/usr/bin/python3
 """python inheritance.
 
 defines and assigns value for parameter
 
 """
+class MetaGeometry(type):
+    """this class overrides the dir init subclass in the class"""
+
+def __dir__(cls):
+    """Magic method that allows to override default"""
 class BaseGeometry:
     """ My BaseGeometry class. """
 
