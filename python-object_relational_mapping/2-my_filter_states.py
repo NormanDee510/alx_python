4daae2d1541a0a3791aac0f1c7a8b3acc38ffe6d
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-20 13:52:48 -0700

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,12 +7,11 @@ def my_filter_states(username, password, database, state_name):
     # Connect to the MySQL server
     db = MySQLdb.connect(
         host="localhost",
-          port=3306, 
-          user=username, 
-          passwd=password, 
-          db=database
-          )
-
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
     # Create a cursor
     cursor = db.cursor()
 
