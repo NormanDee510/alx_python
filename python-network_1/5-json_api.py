0c1d6310011ff1fb22eeb179a998be8a77974b77
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-23 10:52:15 -0700

    Search API

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,20 +1,3 @@
-"""
-Send a POST request to search user by letter and display response information.
-
-This script takes a letter as input, sends a POST request to a URL with the letter as
-a parameter, and displays user information from the response if the JSON is properly
-formatted and not empty.
-
-Usage:
-    python script.py <letter>
-
-Args:
-    letter (str): The letter to search for.
-
-Example:
-    python script.py a
-"""
-
 import requests
 import sys
 
@@ -31,7 +14,10 @@ def search_user_by_letter(letter):
     
     try:
         response = requests.post(url, data=data)
-        response_json = response.json()
+        try:
+            response_json = response.json()
+        except ValueError:
+            response_json = None
         
         if isinstance(response_json, list) and response_json:
             user = response_json[0]
