082f69db81c613e56afef684746183c299eec338
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-23 09:22:41 -0700

    POST an email #1

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,22 +1,6 @@
-"""
-Send a POST request to a given URL with an email parameter and display the response body.
-
-This script takes a URL and an email address as input, sends a POST request to the URL
-with the email as a parameter, and displays the body of the response.
-
-Usage:
-    python script.py <URL> <email>
-
-Args:
-    URL (str): The URL to send the POST request to.
-    email (str): The email address to include in the POST request.
-
-Example:
-    python script.py http://0.0.0.0:5050 with email=test@test.com
-"""
-
 import requests
 import sys
+
 
 def send_post_request(url, email):
     """
@@ -26,18 +10,17 @@ def send_post_request(url, email):
         url (str): The URL to send the POST request to.
         email (str): The email address to include in the POST request.
     """
-    data = {'email': test@test.com}
+    data = {'email': hr@holbertonschool.com}
     try:
         response = requests.post(url, data=data)
         print(response.text)
     except requests.exceptions.RequestException as e:
-        print(f"Email: {email}")
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    if len(sys.argv) == 4 and sys.argv[2] == "with" and sys.argv[3] == "email":
+    if len(sys.argv) == 3:
         url = sys.argv[1]
-        email = sys.argv[4]
-        print(f"request {url} with email={email}\n")
+        email = sys.argv[2]
         send_post_request(url, email)
     else:
-        print("Usage: python script.py <URL> with email=<email>")
+        print("Usage: python script.py <URL> <email>")
