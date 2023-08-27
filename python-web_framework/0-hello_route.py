316897dd51ac7d218bfc328ead8cdd22c4fa2c1d
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-27 00:19:10 -0700

    Hello Flask!

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,10 +1,27 @@
+"""
+This is a simple Flask web application.
+It listens on 0.0.0.0, port 5000 and provides a basic route.
+"""
+
 from flask import Flask
-"""A script that starts a Flask web application"""
+
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Route handler for the root URL.
+    Returns a greeting message.
+    """
     return "Hello HBNB!"
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """
+    Error handler for 404 (Not Found) errors.
+    Returns a custom message for invalid routes.
+    """
+    return "404 Not Found", 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
