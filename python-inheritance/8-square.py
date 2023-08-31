65574081354418377609fb543f506ad10d086707
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-31 12:28:56 -0700

    Square #1

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -11,10 +11,10 @@ class Square(Rectangle):
     """ My Square class that inherits from Rectangle. """
 
     def __init__(self, size):
-        """Instantiates the Square with size."""        
+        """Instantiates the Square with size."""
+        self.__size = size
+        super().integer_validator('size', size)        
         super().__init__(size, size)
 
-    def __str__(self):
-        """Returns a string representation of the Square."""
-        return f"[Square] {self._Rectangle__width}/{self._Rectangle__height}"
+    
         
