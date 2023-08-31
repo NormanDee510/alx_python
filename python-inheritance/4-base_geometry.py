632d3a069cfcc99846da8e71ffcd4c064f4f7dbe
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-31 11:54:19 -0700

    Improve Geometry

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,15 +1,17 @@
-"""python inheritance.
-
-defines and assigns value for parameter
-
-"""
 class MetaGeometry(type):
-    """this class overrides the dir init subclass in the class"""
+    """This class overrides the dir init subclass in the class"""
 
-def __dir__(cls):
-    """Magic method that allows to override default"""
-class BaseGeometry:
-    """ My BaseGeometry class. """
+    def __dir__(cls):
+        """Magic method that allows to override default"""
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+
+class BaseGeometry(metaclass=MetaGeometry):
+    """My BaseGeometry class."""
+
+    def __dir__(cls):
+        """Magic method that allows to override default"""
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
     def area(self):
         """Raises an Exception with the message area() is not implemented."""
