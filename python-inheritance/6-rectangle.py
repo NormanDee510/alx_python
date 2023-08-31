ae83056b1296ff1cede27075510f51647289063d
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-31 12:15:55 -0700

    Rectangle

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,33 +1,10 @@
-#!/usr/bin/python3
+
 """python inheritance.
 
 defines and assigns value for parameter
 
 """
-class BaseGeometry:
-    """ My BaseGeometry class. """
-
-    def area(self):
-        """Raises an Exception with the message area() is not implemented."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates the 'value' argument.
-
-        Args:
-            name (str): The name of the value being validated.
-            value (int): The value to be validated.
-
-        Raises:
-            TypeError: If the 'value' is not an integer.
-            ValueError: If the 'value' is less than or equal to 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """ My Rectangle class that inherits from BaseGeometry. """
@@ -47,3 +24,4 @@ class Rectangle(BaseGeometry):
     def __str__(self):
         """Returns a string representation of the Rectangle."""
         return f"[Rectangle] {self.__width}/{self.__height}"
+    
