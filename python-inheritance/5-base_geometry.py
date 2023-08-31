fc8fecd6fb988c3f5747a101ecc3cee9089a7e10
NormanDee <newman.demba@uhuruinnovative.com>
2023-08-31 12:08:40 -0700

    Integer validator

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,27 +1,30 @@
-#!/usr/bin/python3
 """python inheritance.
 
 defines and assigns value for parameter
 
 """
-class BaseGeometry:
-    """ My BaseGeometry class. """
+class MetaGeometry(type):
+    """This class overrides the dir init subclass in the class"""
+
+    def __dir__(cls):
+        """Magic method that allows to override default"""
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+
+class BaseGeometry(metaclass=MetaGeometry):
+    """My BaseGeometry class."""
+
+    def __dir__(cls):
+        """Magic method that allows to override default"""
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
     def area(self):
         """Raises an Exception with the message area() is not implemented."""
         raise Exception("area() is not implemented")
+       
 
     def integer_validator(self, name, value):
-        """Validates the 'value' argument.
-
-        Args:
-            name (str): The name of the value being validated.
-            value (int): The value to be validated.
-
-        Raises:
-            TypeError: If the 'value' is not an integer.
-            ValueError: If the 'value' is less than or equal to 0.
-        """
+        """Validates the 'value' argument"""       
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
 
