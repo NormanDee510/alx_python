37ff736231a8177feefa6d1a8905a0ee0c99ee26
newman <macnoedy@gmail.com>
2023-09-03 10:46:32 -0700

    First state model

--- python-object_relational_mapping/6-model_state.sql
+++ python-object_relational_mapping/6-model_state.sql
@@ -3,7 +3,26 @@
 Module that defines the State class and creates a database table.
 """
 
--- Create database hbtn_0e_6_usa
-CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
-USE hbtn_0e_6_usa;
-SHOW CREATE TABLE states;
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    """
+    State class represents a database table 'states'.
+
+    Attributes:
+        id (int): An auto-generated, unique integer representing the primary key.
+        name (str): A string representing the name of the state.
+
+    """
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://root:root@localhost/hbtn_0e_6_usa', pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    print("Table 'states' created.")
