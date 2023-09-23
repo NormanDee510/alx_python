94fc2552d906e60ba17a3d50212d4e323fe2ad59
newman <macnoedy@gmail.com>
2023-09-23 11:30:16 -0700

    filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,27 +1,32 @@
 import MySQLdb
 import sys
 
-
 def list_states(username, password, database_name):
     try:
-        # Connecting to the MySQL server
+        # Connect to the MySQL server
         db = MySQLdb.connect(
             host='localhost',
             port=3306,
             user=username,
             passwd=password,
             db=database_name)
-
+        
         """Create a cursor object to interact with the database"""
         cursor = db.cursor()
 
         # Execute the SQL query to retrieve states
         query = "SELECT * FROM states where BINARY name like 'N%' ORDER BY states.id ASC"
         cursor.execute(query)
+
+        # Fetch all the rows
         rows = cursor.fetchall()
+
+        # Display the results
         for row in rows:
             print(row)
-            cursor.close()
+
+        # Close the cursor and the database connection
+        cursor.close()
         db.close()
 
     except MySQLdb.Error as e:
