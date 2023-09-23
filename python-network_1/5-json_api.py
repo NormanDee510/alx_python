30749c5db2fbdeca9f8fdaf223e6c91030f3c589
newman <macnoedy@gmail.com>
2023-09-22 19:49:40 -0700

    json api

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,39 +1,26 @@
-import sys
+
 import requests
+import sys
 
+def main():
+    if len(sys.argv) > 1:
+        letter = sys.argv[1]
+    else:
+        letter = ""
 
-def search_user_by_letter(letter):
-    """
-    Send a POST request to search user by letter and display response information.
-
-    Args:
-        letter (str): The letter to search for.
-    """
     url = "http://0.0.0.0:5000/search_user"
-    
-    
     data = {'q': letter}
-    
+
+    response = requests.post(url, data=data)
+
     try:
-        response = requests.post(url, data=data)
-        try:
-            response_json = response.json()
-        except ValueError:
-            response_json = None
-        
-        if isinstance(response_json, list) and response_json:
-            user = response_json[0]
-            print(f"[{user['id']}] {user['name']}")
-        elif not response_json:
-            print("Not a valid JSON")
+        response_json = response.json()
+        if response_json:
+            print("[{}] {}".format(response_json['id'], response_json['name']))
         else:
-            print("Not a valid JSON")
-    except requests.exceptions.RequestException as e:
-        print("Error:", e)
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        letter = sys.argv[1]
-        search_user_by_letter(letter)
-    else:
-        print("No result")
+    main()
