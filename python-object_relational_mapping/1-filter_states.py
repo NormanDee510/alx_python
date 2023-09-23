44c41c2507abd44a543b1674cad2cd0e73635959
newman <macnoedy@gmail.com>
2023-09-22 23:33:53 -0700

    Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,42 +1,43 @@
-#!/usr/bin/env python3
-import sys
 import MySQLdb
+import sys
 
+def list_states(username, password, database_name):
+    try:
+        # Connect to the MySQL server
+        db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database_name)
+        
+        """Create a cursor object to interact with the database"""
+        cursor = db.cursor()
 
-def filter_states(username, password, database):
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-    cursor = db.cursor()
+        # Execute the SQL query to retrieve states
+        query = "SELECT * FROM states where BINARY name like 'N%' ORDER BY states.id ASC"
+        cursor.execute(query)
 
-    # Execute the query to retrieve states starting with 'N'
-    cursor.execute(
-        "SELECT * FROM states WHERE UPPER(name) LIKE 'N%' "
-        "ORDER BY id ASC"
-    )
+        # Fetch all the rows
+        rows = cursor.fetchall()
 
-    results = cursor.fetchall()
+        # Display the results
+        for row in rows:
+            print(row)
 
-    # Print the results
-    for row in results:
-        print(row)
+        # Close the cursor and the database connection
+        cursor.close()
+        db.close()
 
-    cursor.close()
-    db.close()
-
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Call the function to filter and display the states
-    filter_states(username, password, database)
+        print("Usage: python script.py <username> <password> <database_name>")
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database_name = sys.argv[3]
+        list_states(username, password, database_name)
