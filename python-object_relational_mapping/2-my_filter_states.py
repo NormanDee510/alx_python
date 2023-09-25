4e33a0d502a4ecc86c6c9a54dcabf5f7b729cac9
newman <macnoedy@gmail.com>
2023-09-25 11:48:56 -0700

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -16,8 +16,7 @@ def my_filter_states(username, password, database, state_name):
     cursor = db.cursor()
 
     # Create the SQL query with user input (state name)
-    query = ("SELECT * FROM states WHERE name = %s"
-             "COLLETE utf8mb4_bin ORDER BY states.id")
+    query = ("SELECT * FROM states WHERE name = %s ORDER BY id ASC")
 
     # Execute the query to retrieve states matching the state name
     cursor.execute(query, (state_name,))
