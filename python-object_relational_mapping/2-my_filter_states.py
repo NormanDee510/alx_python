79d5d996dd972b0fd59b870e0717b14456ba849e
newman <macnoedy@gmail.com>
2023-09-25 09:28:03 -0700

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,42 +1,48 @@
-#!/usr/bin/python3
-"""
-Script that takes in an argument and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument.
-"""
-
-import MySQLdb
+#!/usr/bin/env python3
 import sys
+import MySQLdb
+
+
+def my_filter_states(username, password, database, state_name):
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+    # Create a cursor
+    cursor = db.cursor()
+
+    # Create the SQL query with user input (state name)
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+
+    # Execute the query to retrieve states matching the state name
+    cursor.execute(query, (state_name,))
+
+    # Fetch all the results
+    results = cursor.fetchall()
+
+    # Print the results
+    for row in results:
+        print(row)
+
+    # Close the cursor and connection
+    cursor.close()
+    db.close()
 
 if __name__ == "__main__":
-    # Check if all 4 arguments are provided
+    # Check for correct number of arguments
     if len(sys.argv) != 5:
-        print("Usage: {} username password database_name state_name_searched".format(sys.argv[0]))
+        print("Usage: {} <username> <password>"
+              "<database> <state_name>".format(sys.argv[0]))
         sys.exit(1)
-        
+
     username = sys.argv[1]
     password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name_searched = sys.argv[4]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-    try:
-        # Connect to the MySQL server
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
-        cursor = db.cursor()
-
-        # Create and execute the SQL query
-        query = "SELECT * FROM states WHERE name = %s ORDER BY id"
-        cursor.execute(query, (state_name_searched,))
-
-        # Fetch and display the results
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("Error {}: {}".format(e.args[0], e.args[1]))
-        sys.exit(1)
-
-    finally:
-        # Close the database connection
-        if db:
-            db.close()
+    # Call the function to filter and display the states
+    my_filter_states(username, password, database, state_name)
