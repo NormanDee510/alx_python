f53be002dfc8469656e4f6b45f3efd52f56bd071
newman <macnoedy@gmail.com>
2023-09-25 07:59:14 -0700

    model state

--- python-object_relational_mapping/6-model_state.py
+++ python-object_relational_mapping/6-model_state.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python3
-"""
-Script that file that contains the class definition of a State 
+"""Start link class to table in database
 """
 
-from sqlalchemy import Column, Integer, String
+import sys
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
 class State(Base):
-    """this is class which represents the states table in the database"""
+    """State class that inherits from Base"""
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
 
-    def __init__(self, name):
-        self.name = name
+if __name__ == "__main__":
+    # Create a connection to the MySQL database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    # Create the states table
+    Base.metadata.create_all(engine)
+
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Insert a new state into the states table (optional)
+    new_state = State(name='New York')
+    session.add(new_state)
+    session.commit()
+
+    # Close the session
+    session.close()
