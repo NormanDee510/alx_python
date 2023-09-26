6f94a8fbefaac84aee26b8ee82e191778152b127
newman <macnoedy@gmail.com>
2023-09-26 12:13:54 -0700

    model_state

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -17,10 +17,12 @@ class State(Base):
     """
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
+    id = Column(
+        Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql://username:password@localhost:3306/database_name')
+    engine = create_engine(
+        'mysql://username:password@localhost:3306/database_name')
     # Create the 'states' table in the database
     Base.metadata.create_all(engine)
