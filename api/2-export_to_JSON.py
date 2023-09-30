b7152354158cbd6957aca7b42f302574e0252a1e
newman <macnoedy@gmail.com>
2023-09-30 11:03:26 -0700

    Export to JSON

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,44 @@
+import requests
+import sys
+import csv
+
+def get_employee_info(employee_id):
+    # Define the base URL for the API
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    # Make a GET request to get employee details
+    employee_url = f"{base_url}/users/{employee_id}"
+    response = requests.get(employee_url)
+    employee_data = response.json()
+
+    # Make a GET request to get the employee's TODO list
+    todos_url = f"{base_url}/users/{employee_id}/todos"
+    response = requests.get(todos_url)
+    todos_data = response.json()
+
+    # Calculate the number of completed tasks and total tasks
+    completed_tasks = [task for task in todos_data if task['completed']]
+    num_completed_tasks = len(completed_tasks)
+    total_tasks = len(todos_data)
+
+    # Export data to a CSV file
+    csv_filename = f"{employee_id}.csv"
+    with open(csv_filename, mode='w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_MINIMAL)
+        
+        # Write header row
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        
+        # Write data rows
+        for task in todos_data:
+            csv_writer.writerow([employee_id, employee_data['username'], task['completed'], task['title']])
+
+    print(f"Data exported to {csv_filename}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python export_to_CSV.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    get_employee_info(employee_id)
