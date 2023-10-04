9e2a92a9c3dfa580d7ccc3fb195baae6bd09b4d2
newman <macnoedy@gmail.com>
2023-10-04 11:57:36 -0700

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,16 +1,33 @@
-import csv
 import requests
 import sys
-"""
-    Retrieve employee information and TODO list, then export it to a CSV file.
-    Args:
-        employee_id (int): The ID of the employee for whom to retrieve information.
-    Returns:
-        None
+import csv
+
+def check_task_formatting(title):
     """
+    Check the formatting of a task title.
+    
+    Args:
+        title (str): The title of the task.
+
+    Returns:
+        str: "OK" if the formatting is correct (length >= 10), otherwise "Formatting: Incorrect".
+    """
+    if len(title) >= 10:
+        return "OK"
+    else:
+        return "Formatting: Incorrect"
 
 def get_employee_info(employee_id):
-    
+    """
+    Get employee information, TODO list, and export data to a CSV file.
+
+    Args:
+        employee_id (int): The ID of the employee.
+
+    Outputs:
+        CSV file: Records all tasks owned by the employee in the format:
+                  "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+    """
     # Define the base URL for the API
     base_url = "https://jsonplaceholder.typicode.com"
 
@@ -24,26 +41,38 @@ def get_employee_info(employee_id):
     response = requests.get(todos_url)
     todos_data = response.json()
 
-    # Create a CSV file with the specified format
+    # Calculate the number of completed tasks and total tasks
+    completed_tasks = [task for task in todos_data if task['completed']]
+    num_completed_tasks = len(completed_tasks)
+    total_tasks = len(todos_data)
+
+    # Print the employee TODO list progress
+    print(f"Employee {employee_data['name']} is done with tasks({num_completed_tasks}/{total_tasks}):")
+
+    # Print the titles of completed tasks with formatting check
+    for i, task in enumerate(completed_tasks, 1):
+        formatting_check = check_task_formatting(task['title'])
+        print(f"Task {i} {formatting_check}")
+        print(f"    {task['title']}")
+
+    # Export data to a CSV file
     csv_filename = f"{employee_id}.csv"
     with open(csv_filename, mode='w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_MINIMAL)
+        
+        # Write header row
         csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-
-        # Write TODO list data to the CSV file
+        
+        # Write data rows
         for task in todos_data:
             csv_writer.writerow([employee_id, employee_data['username'], task['completed'], task['title']])
 
     print(f"Data exported to {csv_filename}")
 
 if __name__ == "__main__":
-    # Check if the correct number of command-line arguments is provided
     if len(sys.argv) != 2:
-        print("Usage: python export_to_CSV.py <employee_id>")
+        print("Usage: python gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
-    # Parse the employee ID from the command-line argument
     employee_id = int(sys.argv[1])
-
-    # Call the function to retrieve and export employee data
     get_employee_info(employee_id)
