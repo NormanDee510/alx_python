1a78e5aeda9a37cf5d80a648720e63250e8dbbc7
newman <macnoedy@gmail.com>
2023-10-04 12:19:16 -0700

    Export to JSON

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,17 +1,29 @@
-import csv
 import requests
 import sys
+import json
 
 def get_employee_info(employee_id):
     """
-    Retrieve employee information and TODO list, then export it to a CSV file.
+    Retrieve employee information and export data in JSON format.
+
+    This function fetches data about an employee from a REST API, including their TODO list.
+    It then formats this data according to the specified JSON format and exports it to a JSON file.
 
     Args:
-        employee_id (int): The ID of the employee for whom to retrieve information.
+        employee_id (int): The ID of the employee.
 
-    Returns:
-        None
+    Outputs:
+        JSON file: Records all tasks owned by the employee in the following format:
+                   { "USER_ID": [{"task": "TASK_TITLE", "completed": TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]}
+
+    Raises:
+        ValueError: If the provided employee_id is not a valid integer.
     """
+    try:
+        employee_id = int(employee_id)
+    except ValueError:
+        raise ValueError("employee_id must be a valid integer.")
+
     # Define the base URL for the API
     base_url = "https://jsonplaceholder.typicode.com"
 
@@ -25,32 +37,32 @@ def get_employee_info(employee_id):
     response = requests.get(todos_url)
     todos_data = response.json()
 
-    if not todos_data:
-        print("No tasks found for this user.")
-        return
+    # Prepare a dictionary to store the tasks in the specified JSON format
+    user_tasks = {
+        str(employee_id): [
+            {
+                "task": task["title"],
+                "completed": task["completed"],
+                "username": employee_data["username"]
+            }
+            for task in todos_data
+        ]
+    }
 
-    # Create a CSV file with the specified format
-    csv_filename = f"{employee_id}.csv"
-    with open(csv_filename, mode='w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_MINIMAL)
-        
-        # Write header row
-        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        
-        # Write data rows
-        for task in todos_data:
-            csv_writer.writerow([employee_id, employee_data['username'], task['completed'], task['title']])
+    # Print the JSON data for visual verification
+    print(json.dumps(user_tasks, indent=2))
 
-    print(f"Data exported to {csv_filename}")
+    # Export data to a JSON file
+    json_filename = f"{employee_id}.json"
+    with open(json_filename, 'w') as json_file:
+        json.dump(user_tasks, json_file, indent=2)
+
+    print(f"Data exported to {json_filename}")
 
 if __name__ == "__main__":
-    # Check if the correct number of command-line arguments is provided
     if len(sys.argv) != 2:
-        print("Usage: python export_to_CSV.py <employee_id>")
+        print("Usage: python gather_data_from_an_API.py <employee_id>")
         sys.exit(1)
 
-    # Parse the employee ID from the command-line argument
     employee_id = int(sys.argv[1])
-
-    # Call the function to retrieve and export employee data
     get_employee_info(employee_id)
