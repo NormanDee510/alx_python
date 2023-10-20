8f8b28b6d542b777d406556323490575923daf7e
newman <macnoedy@gmail.com>
2023-10-19 23:02:08 -0700

    export to json

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -7,20 +7,38 @@ import sys
 
 if __name__ == "__main__":
     employee_id = sys.argv[1]
-    api_request = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(employee_id))
-    api_request1 = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id))
-    data = api_request.text
-    pjson = json.loads(data)
-    data1 = api_request1.text
-    pjson1 = json.loads(data1)
 
-    #export data to csv data
-    filename = "{}.csv".format(employee_id)
-    with open(filename, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, quoting = csv.QUOTE_ALL)
-        for item in pjson1:
-            user_id = employee_id
-            username = pjson['username']
-            task_completed_status = item['completed']
-            task_title = item['title']
-            writer.writerow([user_id, username, task_completed_status, task_title])
+    # Request employee details and todo list from the API
+    response_user = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    response_todos = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
+
+    if response_user.status_code == 200 and response_todos.status_code == 200:
+        user_data = json.loads(response_user.text)
+        todos_data = json.loads(response_todos.text)
+
+        employee_name = user_data.get("name")
+        user_id = user_data.get("id")
+        total_tasks = len(todos_data)
+        completed_tasks = []
+
+        for task in todos_data:
+            if task["completed"]:
+                completed_tasks.append(task["title"])
+
+        print(f"Employee {employee_name} is done with tasks ({len(completed_tasks)}/{total_tasks}):")
+
+        for task in completed_tasks:
+            print(f"\t{task}")
+
+        # Export data to a CSV file
+        csv_filename = f"{user_id}.csv"
+        with open(csv_filename, mode='w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+            for task in todos_data:
+                task_status = "Completed" if task["completed"] else "Incomplete"
+                csv_writer.writerow([user_id, employee_name, task_status, task["title"]])
+
+        print(f"Data exported to {csv_filename}")
+    else:
+        print("Failed to retrieve data. Please check the employee ID and API availability.")
